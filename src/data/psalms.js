549230503c6596d9c48b7836d997e84010b80e82
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2023-06-12 21:55:35 +0200

    Added psalms.

--- src/data/psalms.js
+++ src/data/psalms.js
@@ -1,53 +1,46 @@
  const psalms =[
 {
     id:1,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "148,1-2. 3-4. 9-10. 11-13",
+    slug: "Niech imię Pana będzie pochwalone.",
+    content: "Chwalcie Pana z niebios, \n chwalcie Go na wysokościach. \n Chwalcie Go wszyscy Jego aniołowie, \n chwalcie Go wszystkie Jego zastępy \n  Refren \n Chwalcie Go słońce i księżycu, \n chwalcie Go wszystkie gwiazdy świecące. \n Chwalcie Go nieba najwyższe \n i wody, co są ponad niebem. \n Refren \n Góry i wszelkie pagórki, \n drzewa rodzące owoc i wszystkie cedry, \n Dzikie zwierzęta i wszelkie bydło, \n płazy i ptactwo skrzydlate. \n Refren \n Królowie ziemscy i wszystkie narody, \n władcy i wszyscy sędziowie tej ziemi, \n Młodzieńcy i dziewice, starcy i dzieci \n niech imię Pana wychwalają \n bo tylko Jego imię jest wzniosłe. \n Refren \n" 
 },
 {
     id:2,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "Ps 112[111], 1-2. 3-4. 7b-9",
+    slug: "Radość znajduję w Pańskich przykazaniach.",
+    content: "Błogosławiony człowiek, który boi się Pana \n i wielką radość znajduje w Jego przykazaniach. \n potomstwo jego będzie potężne na ziemi, \n dostąpi błogosławieństwa pokolenie prawych. \n Refren \n Dobrobyt i bogactwo będą w jego domu, \n a jego sprawiedliwość będzie trwała zawsze. \n On wschodzi w ciemnościach jak światło dla prawych, \n łagodny, miłosierny i sprawiedliwy. \n Refren \n Jego mocne serce zaufało Panu, \n jego wierne serce lękać się nie będzie, \n i spojrzy z wysoka na swoich przeciwników. \n Rozdaje i obdarza ubogich, \n jego sprawiedliwość będzie trwała zawsze, \n wywyższona z chwałą będzie jego potęga. \n Refren \n" 
 },
 {
     id:3,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "Ps 34[33], 2-3. 4-5. 8-9",
+    slug: " Przez całe życie będę sławił Pana.",
+    content: "Będę błogosławił Pana po wieczne czasy, \n jego chwała będzie zawsze na moich ustach. \n Dusza moja chlubi się Panem, \n niech słyszą to pokorni i niech się weselą. \n Refren \n Wysławiajcie razem ze mną Pana, \n wspólnie wywyższajmy Jego imię. \n Szukałem pomocy u Pana, a On mnie wysłuchał \n i wyzwolił od wszelkiej trwogi. \n Refren \n Anioł Pański otacza szańcem bogobojnych, \n aby ich ocalić. \n Skosztujcie i zobaczcie, jak Pan jest dobry, \n szczęśliwy człowiek, który znajduje w Nim ucieczkę. \n Refren \n" 
 },
 {
     id:4,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "Ps 128[127], 1-2. 3. 4-5",
+    slug: "Błogosławiony, kto się boi Pana.",
+    content: "Błogosławiony jest każdy, kto się boi Pana \n i chodzi jego drogami. \n Będziesz spożywał owoc pracy rąk swoich, \n szczęście osiągniesz i dobrze ci będzie. \n Refren \n Małżonka twoja jest jak płodny szczep winny \n w zaciszu twojego domu. \n Synowie twoi jak oliwne gałązki \n dokoła twego stołu. \n Refren \n Tak będzie błogosławiony człowiek, \n który boi się Pana! \n Niech cię z Syjonu Pan błogosławi \n i obyś oglądał pomyślność Jeruzalem \n przez wszystkie dni twego życia. \n Refren" 
 },
 {
     id:5,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "Ps 103[102], 1-2. 8 i 13. 17-18a",
+    slug: "Chwal i błogosław, duszo moja, Pana.",
+    content: "Błogosław, duszo moja, Pana, * \n  i wszystko, co jest we mnie, święte imię Jego. \n Błogosław, duszo moja, Pana * \n i nie zapominaj o wszystkich Jego dobrodziejstwach. \n  On odpuszcza wszystkie twoje winy * \n i leczy wszystkie choroby, \n On twoje życie ratuje od zguby, * \n obdarza cię łaską i zmiłowaniem. \n Miłosierny jest Pan i łaskawy, * \n nieskory do gniewu i bardzo cierpliwy. \n Nie zapamiętuje się w sporze, * \n nie płonie gniewem na wieki. \n Jak ojciec lituje się nad dziećmi, * \n tak Pan się lituje nad tymi, którzy się Go boją. \n Wie On, z czegośmy powstali, * \n pamięta, że jesteśmy prochem. \n Łaska zaś Boga jest wieczna dla Jego wyznawców, * \n a Jego sprawiedliwość nad ich potomstwem, \n nad wszystkimi, którzy strzegą Jego przymierza * \n i pamiętają, by spełniać Jego przykazania." 
 },
 {
     id:6,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "Ps 145[144], 8-9. 10 i 15. 17-18",
+    slug: "Nasz Pan jest dobry dla wszystkich swych dzieci.",
+    content: "Pan jest łagodny i miłosierny, \n nieskory do gniewu i bardzo łaskawy. \n Pan jest dobry dla wszystkich, \n a Jego miłosierdzie nas wszystkim, co stworzył. \n Refren \n Niech Cię wielbią, Panie, wszystkie Twoje dzieła \n i niech Cię błogosławią Twoi święci. \n Oczy wszystkich zwracają się ku Tobie, \n a Ty karmisz ich we właściwym czasie. \n Refren \n Pan jest sprawiedliwy na wszystkich swych drogach \n  i łaskawy we wszystkich swoich dziełach. \n Pan jest blisko wszystkich, którzy Go wzywają, \n wszystkich wzywających Go szczerze. \n Refren" 
 },
 {
     id:7,
-    title: "",
-    slug: "",
-    content: "" 
-},
-{
-    id:8,
-    title: "",
-    slug: "",
-    content: "" 
+    title: "Ps 33[32], 12-13. 18 i 20. 21-22",
+    slug: "Pełna jest ziemia łaskawości Pana.",
+    content: "Błogosławiony lud, którego Pan jest Bogiem, \n naród, który On wybrał na dziedzictwo dla siebie. \n Pan spogląda z nieba, \n widzi wszystkich ludzi. \n Refren \n Oczy Pana zwrócone na bogobojnych \n na tych, którzy czekają na jego łaskę. \n Dusza nasza oczekuje Pana, \n On jest naszą pomocą i tarczą. \n Refren \n Raduje się w nim nasze serce, \n ufamy jego świętemu imieniu. \n Panie, niech nas ogarnie Twoja łaska, \n według nadziei, którą pokładamy w Tobie." 
 }
-
 ]
 
 
