1d145f474c2677a580a199f44deaf3e79ceac4e4
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2023-06-28 20:53:11 +0200

    Added new page.

--- src/data/firstDance.js
+++ src/data/firstDance.js
@@ -2,357 +2,364 @@
 {
     id:1,
     title: "Zabiorę Cię Właśnie tam",
-    wykonawca: "Kancelaria",
+    band: "Kancelaria",
     url: "https://www.youtube.com/watch?v=KvL0tXRmQw4", 
     rok: ""   
 },
 {
     id:2,
     title: "Moja miłość największa",
-    wykonawca: "M. Bajor",
+    band: "M. Bajor",
     url: "https://www.youtube.com/watch?v=A_7Ct_hVZO0", 
     rok: ""   
 },
 {
     id:3,
     title: "Prócz ciebie nic",
-    wykonawca: "Kayah i Kiliyański",
+    band: "Kayah i Kiliyański",
     url: "https://www.youtube.com/watch?v=tc-nrYjJUeM", 
     rok: ""   
 },{
     id:4,
     title: "Wymyśliłam cię",
-    wykonawca: "Irena Jarocka",
+    band: "Irena Jarocka",
     url: "https://www.youtube.com/watch?v=U0yg7DSM6QA", 
     rok: ""   
 },{
     id:5,
     title: "Niebieska Piosenka",
-    wykonawca: "Grzegorz Tomczak",
+    band: "Grzegorz Tomczak",
     url: "https://www.youtube.com/watch?v=1Mzaphj9eCg", 
     rok: ""   
 },{
     id:6,
     title: "Jesteś moim niebem",
-    wykonawca: "4You",
+    band: "4You",
     url: "https://www.youtube.com/watch?v=4RL8M3Uiw8A", 
     rok: ""   
 },{
     id:7,
     title: "Kocham Cię",
-    wykonawca: "Chłopcy Z Placu Broni",
+    band: "Chłopcy Z Placu Broni",
     url: "https://www.youtube.com/watch?v=n7fXVp9YNQw", 
     rok: ""   
 },{
     id:8,
     title: "Because You loved me",
-    wykonawca: "Celine Dion",
+    band: "Celine Dion",
     url: "https://www.youtube.com/watch?v=O3XGynvy_V4", 
     rok: ""   
 },{
     id:9,
     title: "Chce tu zostać",
-    wykonawca: "Farba",
+    band: "Farba",
     url: "https://www.youtube.com/watch?v=yihUfPJ_WiY", 
     rok: ""   
 },{
     id:10,
     title: "Say you say me",
-    wykonawca: "Lionel Richie",
+    band: "Lionel Richie",
     url: "https://www.youtube.com/watch?v=9OfoTaLXrUo", 
     rok: ""   
 },{
     id:11,
     title: "Zakochani",
-    wykonawca: "Natalia Kukulska",
+    band: "Natalia Kukulska",
     url: "https://www.youtube.com/watch?v=cin-TvWMYew", 
     rok: ""   
 },{
     id:12,
     title: "Będziesz moją panią",
-    wykonawca: "Marek Grechuta",
+    band: "Marek Grechuta",
     url: "https://www.youtube.com/watch?v=XWgehPzY-rE", 
     rok: ""   
 },{
     id:13,
     title: "Od dziś",
-    wykonawca: "Paula",
+    band: "Paula",
     url: "https://www.youtube.com/watch?v=fwD8d1T2gow", 
     rok: ""   
 },{
     id:14,
     title: "Przetańczyć z Tobą chcę...",
-    wykonawca: "Anna Jantar",
+    band: "Anna Jantar",
     url: "https://www.youtube.com/watch?v=E-zzfmSdjl8", 
     rok: ""   
 },{
     id:15,
     title: "Wielka miłość",
-    wykonawca: "Seweryn Krajewski",
-    url: "", 
+    band: "Seweryn Krajewski",
+    url: "https://www.youtube.com/watch?v=wJElyE4j1Y0", 
     rok: ""   
 },{
     id:16,
     title: "Z kimś takim jak ty",
-    wykonawca: "Kindla",
-    url: "", 
+    band: "Kindla",
+    url: "https://www.youtube.com/watch?v=9fBv875D8PQ", 
     rok: ""   
 },{
     id:17,
     title: "O miłości",
-    wykonawca: "Wilki",
-    url: "", 
+    band: "Robert Gawliński",
+    url: "https://www.youtube.com/watch?v=O-HSrImBiUM", 
     rok: ""   
 },{
     id:18,
     title: "Masters",
-    wykonawca: "Żono moja",
-    url: "", 
+    band: "Żono moja",
+    url: "https://www.youtube.com/watch?v=J8t9d4TIVHQ", 
     rok: ""   
 },{
     id:19,
     title: "Truly madly deeply",
-    wykonawca: "Savage Garden",
-    url: "", 
+    band: "Savage Garden",
+    url: "https://www.youtube.com/watch?v=WQnAxOQxQIU", 
     rok: ""   
 },{
     id:20,
     title: "Za jeden uśmiech Twój",
-    wykonawca: "Sumptuastic",
-    url: "", 
+    band: "Sumptuastic",
+    url: "https://www.youtube.com/watch?v=qAns2c3cxuc", 
     rok: ""   
 },{
     id:21,
     title: "W perły zmienić deszcz",
-    wykonawca: "Universe",
-    url: "", 
+    band: "Universe",
+    url: "https://www.youtube.com/watch?v=fygkjJrlmCY", 
     rok: ""   
 },{
     id:22,
     title: "Love is around",
-    wykonawca: "Wet Wet Wet",
-    url: "", 
+    band: "Wet Wet Wet",
+    url: "https://www.youtube.com/watch?v=h3gEkwhdXUE", 
     rok: ""   
 },{
     id:23,
     title: "Zawsze będę kochał cię",
-    wykonawca: "Tomasz Gralewicz",
-    url: "", 
+    band: "Tomasz Gralewicz",
+    url: "https://www.youtube.com/watch?v=ii3-aYRSDtY", 
     rok: ""   
 },{
     id:24,
     title: "Za Tobą pójdę jak na bal",
-    wykonawca: "Krzysztof Krawczyk",
-    url: "", 
-    rok: ""   
+    band: "Krzysztof Krawczyk",
+    url: "https://www.youtube.com/watch?v=97mN7Wbr8ek", 
+    rok: "1986"   
 },{
     id:25,
     title: "Bo jesteś ty",
-    wykonawca: "Krzysztof Krawczyk",
-    url: "", 
+    band: "Krzysztof Krawczyk",
+    url: "https://www.youtube.com/watch?v=KOY_05KwJYk", 
     rok: ""   
 },
 {
     id:26,
     title: "Szary świat",
-    wykonawca: "Sanah i Kwiat Jabłoni",
+    band: "Sanah i Kwiat Jabłoni",
     url: "https://www.youtube.com/watch?v=Ro9efOXHQpY", 
     rok: ""   
 },
 {
     id:27,
     title: "Nic dwa razy",
-    wykonawca: "Sanah (W. Szymborska) ",
+    band: "Sanah (W. Szymborska) ",
     url: "https://www.youtube.com/watch?v=5wgOm1O6y2o", 
     rok: ""   
 },
 {
     id:28,
     title: "You are the reason",
-    wykonawca: "Scott&Lewis",
-    url: "", 
+    band: "Calum Scott",
+    url: "https://www.youtube.com/watch?v=ShZ978fBl6Y", 
     rok: ""   
 },
 {
     id:29,
     title: "Nothing's gonna change my love for You",
-    wykonawca: "George Benson",
+    band: "George Benson",
     url: "https://www.youtube.com/watch?v=SWcbed87wkI", 
     rok: ""   
 },
 {
     id:30,
     title: "Życie jest piękne",
-    wykonawca: "Golec uOrkiestra",
+    band: "Golec uOrkiestra",
     url: "https://www.youtube.com/watch?v=MkkwvndQxk0", 
     rok: ""   
 },
 {
     id:31,
     title: "I Love You Baby",
-    wykonawca: "Frank Sinatra",
-    url: "", 
+    band: "Frank Sinatra",
+    url: "https://www.youtube.com/watch?v=AiIBKcd4m5Q", 
     rok: ""   
 },
 {
     id:32,
     title: "I Have Nothing",
-    wykonawca: "Whitney Houston",
-    url: "", 
+    band: "Whitney Houston",
+    url: "https://www.youtube.com/watch?v=FxYw0XPEoKE", 
     rok: ""   
 },
 {
     id:33,
     title: "Can You Feel The Love Tonight",
-    wykonawca: "Elton John",
+    band: "Elton John",
     url: "https://www.youtube.com/watch?v=rt7-GSlpUjU", 
     rok: ""   
 },
 {
     id:34,
     title: "Ti Amo",
-    wykonawca: "Umberto Tozzi & Monica Bellucci",
+    band: "Umberto Tozzi & Monica Bellucci",
     url: "https://www.youtube.com/watch?v=VrqwVB1ZvZ4", 
     rok: ""   
 },
 {
     id:35,
     title: "Crazy in love | 50 Shades of Grey",
-    wykonawca: "Sofia Karlberg",
+    band: "Sofia Karlberg",
     url: "https://www.youtube.com/watch?v=Ius891Yf6lw", 
     rok: ""   
 },
 {
     id:36,
     title: "Say you love me",
-    wykonawca: "Jessie Ware",
+    band: "Jessie Ware",
     url: "https://www.youtube.com/watch?v=WICVR0071WU&t=0s", 
     rok: ""   
 },
 {
     id:37,
     title: "Can't take my eyes off You",
-    wykonawca: "Morten Harket",
+    band: "Morten Harket",
     url: "https://www.youtube.com/watch?v=CS3W5HNDjl8&t=0s", 
     rok: ""   
 },
 {
     id:38,
     title: "Conversations in the Dark",
-    wykonawca: "John Legend",
+    band: "John Legend",
     url: "https://www.youtube.com/watch?v=Jx1PhwTT2qU&t=0s", 
     rok: ""   
 },
 {
     id:39,
     title: "Stumblin' in",
-    wykonawca: "Chris Norman & Suzi Quatro",
+    band: "Chris Norman & Suzi Quatro",
     url: "https://www.youtube.com/watch?v=cHg4qThHveU&t=0s", 
     rok: ""   
 },
 {
     id:40,
     title: "LOVER",
-    wykonawca: "Taylor Swift",
+    band: "Taylor Swift",
     url: "https://www.youtube.com/watch?v=dIUkyatLWME&t=0s", 
     rok: ""   
 },
 {
     id:41,
     title: "NEVER ENOUGH",
-    wykonawca: "The Greatest Showman",
+    band: "The Greatest Showman",
     url: "https://www.youtube.com/watch?v=DlOHWyTMRko&t=0s", 
     rok: ""   
 },
 {
     id:42,
     title: "Dancing in the Moonlight",
-    wykonawca: "TOPLOADER",
+    band: "TOPLOADER",
     url: "https://www.youtube.com/watch?v=JPXehbKMpc8&t=0s", 
     rok: ""   
 },
 {
     id:43,
     title: "YOU ARE THE REASON",
-    wykonawca: "Calum Scott",
+    band: "Calum Scott",
     url: "https://www.youtube.com/watch?v=V45UIQzQ0Fg&t=0s", 
     rok: ""   
 },
 {
     id:44,
     title: "TIME OF MY LIFE",
-    wykonawca: "Dirty Dancing",
+    band: "Dirty Dancing",
     url: "https://www.youtube.com/watch?v=jQPqC91Tf0M&t=0s", 
     rok: ""   
 },
 {
     id:45,
     title: "You Raise Me Up",
-    wykonawca: "Josh Groban",
+    band: "Josh Groban",
     url: "https://www.youtube.com/watch?v=xPRF6FeMb7g&t=0s", 
     rok: ""   
 },
 {
     id:46,
     title: "Marry You",
-    wykonawca: "Bruno Mars",
+    band: "Bruno Mars",
     url: "https://www.youtube.com/watch?v=HOdSwKp1vQs", 
     rok: ""   
 },
 {
     id:47,
     title: "Just The Way You Are",
-    wykonawca: "Bruno Mars",
+    band: "Bruno Mars",
     url: "https://www.youtube.com/watch?v=PgboZJWEz4w", 
     rok: ""   
 },
 {
     id:48,
     title: "Can't help falling in love",
-    wykonawca: "Elvis Presley",
-    url: "", 
+    band: "Elvis Presley",
+    url: "https://www.youtube.com/watch?v=vGJTaP6anOU", 
     rok: ""   
 },
 {
     id:49,
     title: "Za daleko",
-    wykonawca: "Mrozu & Vito Bambino",
+    band: "Mrozu & Vito Bambino",
     url: "https://www.youtube.com/watch?v=lyjitPZqdxg", 
     rok: ""   
 },
 {
     id:50,
     title: "TO CO MASZ TY!",
-    wykonawca: "Dawid Podsiadło",
+    band: "Dawid Podsiadło",
     url: "https://www.youtube.com/watch?v=zYm9KJrZT38", 
     rok: ""   
 },
 {
     id:51,
     title: "Kiedy tylko spojrzę",
-    wykonawca: "Sylwia Grzeszczak",
+    band: "Sylwia Grzeszczak",
     url: "https://www.youtube.com/watch?v=7QaZLC2Xmpw", 
     rok: ""   
 },
 {
     id:52,
     title: "Power of Love",
-    wykonawca: "Celine Dion",
+    band: "Celine Dion",
     url: "https://www.youtube.com/watch?v=HpySPX8iad4", 
     rok: ""   
 },
 {
     id:53,
     title: "Najlepszy dzień w moim życiu",
-    wykonawca: "Sanah",
+    band: "Sanah",
     url: "https://www.youtube.com/watch?v=m7iJl-fTGls", 
     rok: ""   
 },
 {
     id:53,
     title: "Lubię wracać tam gdzie byłem",
-    wykonawca: "Zbigniew Wodecki",
+    band: "Zbigniew Wodecki",
     url: "https://www.youtube.com/watch?v=7Rx9gtWgn_w", 
+    rok: ""   
+},
+{
+    id:53,
+    title: "Ten Stan",
+    band: "Sanah",
+    url: "https://www.youtube.com/watch?v=KYMGNHGfqrI", 
     rok: ""   
 }
 ]
---
+++ src/views/FirstDanceList.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import SlotCardList from '@/components/Slot-cardList.vue';
+import firstDanceMusic from '@/data/firstDance.js';
+const items: Object | any = firstDanceMusic
+</script>
+<template >
+    <div class="wrapper">
+        <p class="title">Muzyka najczęściej podczas pierwszego tańca Pary Młodej</p>
+        <SlotCardList v-for="item in items" :key="item.id">
+            <div class="container">
+                <div class="itemId">{{item.id}}</div>
+                <div class="itemBand">{{item.band}}</div>
+                <div class="itemTitle">{{item.title}}</div>
+                <div class="itemUrl">                    
+                    <a :href="item.url" target="_blank">play</a>
+                </div>
+                
+            </div>
+        </SlotCardList>
+    </div>
+</template>
+<style scoped>
+.title {
+    width: 100%;
+    text-align: center;
+    margin: 50px 0;
+    font-size: 26px;
+    font-weight: 600;
+}
+.wrapper {
+    margin: 70px 0;   
+}
+.container {
+        display: flex;
+        flex-direction: row; 
+        justify-content: center;      
+        align-items: center;
+        border-bottom: 1px solid rebeccapurple;
+        min-height: 50px;
+        height: auto; 
+        width: 70%; 
+    }
+    .container:last-child {border-bottom: none}
+.itemId, .itemUrl {
+    width: 70px;
+}
+.itemTitle {
+    width: 50%;
+}
+.itemBand {
+    width: 50%;
+}
+
+</style>
